fc08d6ea588a64362da8a6b9c589225c6e8a3d3a
JasmineS <Jasmine.See@wexinc.com>
2025-02-18 16:05:00 +1100

    Create properties for the three main entities

---
+++ CourseMapping.Domain/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMapping.Domain
+{
+    internal class Course
+    {
+        // Properties
+        public string Name { get;  set; } 
+
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public List<Unit> Units { get; set; }
+    }
+}
---
+++ CourseMapping.Domain/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMapping.Domain
+{
+    internal class Unit
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public int Level { get; set; }
+    }
+}
