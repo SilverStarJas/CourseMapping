5918109fff1e97faa919f1adfbe1a9298234082c
JasmineS <Jasmine.See@wexinc.com>
2025-07-15 10:20:34 +1000

    Fix component tests

--- CourseMapping.Tests.ComponentTests/Endpoints/UniversityEndpoints/UniversityTests.cs
+++ CourseMapping.Tests.ComponentTests/Endpoints/UniversityEndpoints/UniversityTests.cs
@@ -5,13 +5,14 @@ using CourseMapping.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CourseMapping.Tests.ComponentTests.Fixtures;
+using CourseMapping.Web.Models;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Xunit;
 
 namespace CourseMapping.Tests.ComponentTests.Endpoints.UniversityEndpoints;
 
-public class UniversityTests
+public class UniversityTests : IAsyncLifetime
 {
     private readonly HttpClient _client;
     private readonly ApplicationDbContext _dbContext;
@@ -22,111 +23,113 @@ public class UniversityTests
         {
             builder.ConfigureServices(services =>
             {
-                var dbContextDescriptor = services.Where(
-                    d => d.ServiceType == 
-                         typeof(IDbContextOptionsConfiguration<ApplicationDbContext>)).ToList();
+                var dbContextDescriptor = services
+                    .Where(d => d.ServiceType == typeof(IDbContextOptionsConfiguration<ApplicationDbContext>))
+                    .ToList();
 
                 foreach (var descriptor in dbContextDescriptor)
                     services.Remove(descriptor);
-                
+
                 services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseInMemoryDatabase("TestDb"));
             });
         });
-        
+
         var scopeFactory = webAppFactory.Services.GetRequiredService<IServiceScopeFactory>();
         var scope = scopeFactory.CreateScope();
         _dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         _client = webAppFactory.CreateClient();
-        _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(Guid.NewGuid().ToString());
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue(Guid.NewGuid().ToString());
     }
+
+    public async Task InitializeAsync()
+    {
+        _dbContext.Universities.RemoveRange(_dbContext.Universities);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
 
     [Fact]
     public async Task GivenAValidUniversityId_WhenGetUniversityEndpointIsCalled_ReturnAUniversityCorrectly()
     {
-        // Arrange
         var university = new University(Guid.CreateVersion7(), "Test University", "Australia");
         _dbContext.Universities.Add(university);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
 
-        // Act
         var response = await _client.GetAsync($"v1/universities/{university.Id}");
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         var content = await response.Content.ReadAsStringAsync();
         content.Should().Contain("Test University");
         content.Should().Contain("Australia");
     }
-    
+
     [Fact]
     public async Task GivenAnInvalidUniversityId_WhenGetUniversityEndpointIsCalled_ReturnNotFound()
     {
-        // Arrange
         var invalidId = Guid.CreateVersion7();
 
-        // Act
         var response = await _client.GetAsync($"v1/universities/{invalidId}");
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
-    
+
     [Fact]
     public async Task GivenUniversitiesExist_WhenGetAllUniversitiesEndpointIsCalled_ReturnAllUniversities()
     {
-        // Arrange
         var university1 = new University(Guid.CreateVersion7(), "University One", "Country A");
         var university2 = new University(Guid.CreateVersion7(), "University Two", "Country B");
         _dbContext.Universities.AddRange(university1, university2);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
 
-        // Act
         var response = await _client.GetAsync("v1/universities");
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var content = await response.Content.ReadFromJsonAsync<List<University>>();
-        content.Should().Contain(university1);
-        content.Should().Contain(university2);
-        content.Count.Should().Be(2);
+        _dbContext.Universities.Should().Contain(university1);
+        _dbContext.Universities.Should().Contain(university2);
+        _dbContext.Universities.Count().Should().Be(2);
     }
 
     [Fact]
     public async Task GivenValidInput_WhenUpdateUniversityEndpointIsCalled_ReturnUpdatedUniversity()
     {
-        // Arrange
         var university = new University(Guid.CreateVersion7(), "Old Name", "Old Country");
         _dbContext.Universities.Add(university);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
 
-        var updateRequest = new { Name = "New Name", Country = "New Country" };
+        var updateRequest = new UpdateUniversityRequest
+        {
+            Name = "New Name",
+            Country = "New Country"
+        };
 
-        // Act
-        var response = await _client.PutAsJsonAsync($"v1/universities/{university.Id}", updateRequest);
+        var updateResponse = await _client.PutAsJsonAsync($"v1/universities/{university.Id}", updateRequest);
+        updateResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
 
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        var response = await _client.GetAsync($"v1/universities/{university.Id}");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        university.Name.Should().Be("New Name");
-        university.Country.Should().Be("New Country");
+        var updatedUniversity = await response.Content.ReadFromJsonAsync<University>();
+        updatedUniversity.Should().NotBeNull();
+        updatedUniversity!.Id.Should().Be(university.Id);
+        updatedUniversity.Name.Should().Be("New Name");
+        updatedUniversity.Country.Should().Be("New Country");
     }
-    
+
     [Fact]
     public async Task GivenAValidUniversityId_WhenDeleteUniversityEndpointIsCalled_ReturnNoContent()
     {
-        // Arrange
         var university = new University(Guid.CreateVersion7(), "Test University", "Australia");
         _dbContext.Universities.Add(university);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
 
-        // Act
         var response = await _client.DeleteAsync($"v1/universities/{university.Id}");
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-        
-        var content = await response.Content.ReadFromJsonAsync<List<University>>();
+
+        var content = _dbContext.Universities.ToList();
         content.Should().BeEmpty();
     }
 }
