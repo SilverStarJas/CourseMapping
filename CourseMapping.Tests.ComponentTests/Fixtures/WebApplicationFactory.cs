261bf211b02194213605c41dd16a553c6bceb688
JasmineS <Jasmine.See@wexinc.com>
2025-12-09 11:09:08 +1100

    test workflow #2

--- CourseMapping.Tests.ComponentTests/Fixtures/WebApplicationFactory.cs
+++ CourseMapping.Tests.ComponentTests/Fixtures/WebApplicationFactory.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace CourseMapping.Tests.ComponentTests.Fixtures;
 
